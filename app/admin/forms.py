c3f7fd9f9b30dd014e579d33191f2ff2ed281447
Gabriel Muj <muj_gabriel@yahoo.com>
2016-04-16 18:05:57 +0300

    Add detail page for agency

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,9 +1,17 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required, Length
+from wtforms.validators import Length, DataRequired
 
 
-class AgencyForm(Form):
-    name = StringField('Name', validators=[Required(), Length(1, 64)])
-    address = StringField('Address', validators=[Required(), Length(1, 64)])
+class BaseAgencyForm(Form):
+    name = StringField('Name', validators=[DataRequired(), Length(1, 64)])
+    address = StringField('Address', validators=[DataRequired(), Length(1, 64)])
+
+
+class AddAgencyForm(BaseAgencyForm):
     submit = SubmitField('Add agency')
+
+
+class EditAgencyForm(BaseAgencyForm):
+    update = SubmitField('Update agency')
+    delete = SubmitField('Delete agency')
--- app/admin/views.py
+++ app/admin/views.py
@@ -4,13 +4,13 @@ from flask.ext.login import login_required
 from . import admin
 from .. import db
 from ..models import Agency
-from .forms import AgencyForm
+from .forms import AddAgencyForm, EditAgencyForm
 
 
 @admin.route('/agency/add', methods=['GET', 'POST'])
 @login_required
 def add_agency():
-    form = AgencyForm()
+    form = AddAgencyForm()
     if form.validate_on_submit():
         agency = Agency(name=form.name.data,
                         address=form.address.data)
@@ -19,6 +19,23 @@ def add_agency():
         flash('Agency {} was added to database.'.format(agency.name))
         return redirect(url_for('main.index'))
     return render_template("admin/add_agency.html", form=form)
+
+@admin.route('/agency/detail/<int:agency_id>', methods=['GET', 'POST'])
+@login_required
+def detail_agency(agency_id):
+    agency = Agency.query.get_or_404(agency_id)
+    form = EditAgencyForm(obj=agency)
+    if form.validate_on_submit():
+        if form.update.data:
+            form.populate_obj(agency)
+            flash('Agency {} was updated successfully.'.format(agency.name))
+        elif form.delete.data:
+            db.session.delete(agency)
+            db.session.commit()
+            flash('Agency {} was deleted successfully.'.format(agency.name))
+        return redirect(url_for("admin.list_agencies"))
+
+    return render_template("admin/detail_agency.html", form=form)
 
 
 @admin.route('/agency/list', methods=['GET'])
